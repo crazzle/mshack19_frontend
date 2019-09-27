9068be2b79a9aaab15a401961cb7a3c46990c3fc
philipp.wolters <philipp.wolters@codecentric.de>
2019-09-27 17:10:57 +0200

    add mock data to service

--- src/src/app/service/home.service.ts
+++ src/src/app/service/home.service.ts
@@ -6,11 +6,11 @@ import {Feature} from '../models/Feature';
 })
 export class HomeService {
   features: Feature[] = [
-    {name: 'Nähe zur Uni', priorities: ['<= 1km', '<= 5km', '>5km'], value: 1},
-    {name: 'Einkaufmöglichkeiten', priorities: ['L', 'M', 'H'], value: 1},
-    {name: 'Nightlife', priorities: ['keine', 'wenige Bars', 'Altstadt'], value: 2},
-    {name: 'ÖPNV', priorities: ['<= 50m', '<= 200m', '>200m'], value: 2},
-    {name: 'Mietspiegel', priorities: ['niedrig', 'hoch'], value: 1}
+    {name: 'Nähe zur Uni', priorities: ['<= 1km', '<= 5km', '>5km'], value: 1, description: 'Lorem Ipsum'},
+    {name: 'Einkaufmöglichkeiten', priorities: ['L', 'M', 'H'], value: 1, description: 'Lorem Ipsum'},
+    {name: 'Nightlife', priorities: ['keine', 'wenige Bars', 'Altstadt'], value: 2, description: 'Lorem Ipsum'},
+    {name: 'ÖPNV', priorities: ['<= 50m', '<= 200m', '>200m'], value: 2, description: 'Lorem Ipsum'},
+    {name: 'Mietspiegel', priorities: ['niedrig', 'hoch'], value: 1, description: 'Lorem Ipsum'}
   ];
 
   constructor() {
