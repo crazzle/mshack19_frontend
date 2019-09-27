5ab2d20f001d309132d454fb49a67afd62a3f9a9
philipp.wolters <philipp.wolters@codecentric.de>
2019-09-27 16:13:46 +0200

    Add feature model and pipe to convert selection to value

--- src/src/app/helper/selection.pipe.ts
+++ src/src/app/helper/selection.pipe.ts
@@ -1,12 +1,11 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'selection'
 })
 export class SelectionPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: number, list: string[]): any {
+    return list[value];
   }
-
 }
---
+++ src/src/app/models/Feature.ts
@@ -0,0 +1,6 @@
+export interface Feature {
+  name: string;
+  priorities: string[];
+  value: number;
+  logo?: object;
+}
