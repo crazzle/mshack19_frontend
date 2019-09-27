51c1cfaeb7d5fad9de457a0b72985c1eafdd933d
philipp.wolters <philipp.wolters@codecentric.de>
2019-09-27 21:59:07 +0200

    --- FIX MERGE ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { FeatureSelectionComponent } from './feature-selection/feature-selection.component';
-import { FeatureSelectionItemComponent } from './feature-selection/feature-selection-item/feature-selection-item.component';
-import { HeatMapComponent } from './heatmap/heat-map.component';
-import { ResultListComponent } from './result-list/result-list.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {FeatureSelectionComponent} from './feature-selection/feature-selection.component';
+import {FeatureSelectionItemComponent} from './feature-selection/feature-selection-item/feature-selection-item.component';
+import {ResultListComponent} from './result-list/result-list.component';
 import {AppRoutingModule} from './app-routing.module';
-import { MatCardModule, MatListModule, MatSliderModule, MatButtonModule} from '@angular/material';
-import { SelectionPipe } from './helper/selection.pipe';
-import { ContainerComponent } from './container/container.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import {MatButtonModule, MatCardModule, MatListModule, MatSliderModule} from '@angular/material';
+import {SelectionPipe} from './helper/selection.pipe';
+import {ContainerComponent} from './container/container.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {HeatMapComponent} from './heatmap/heat-map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingPageComponent,
-    FeatureSelectionComponent,
-    FeatureSelectionItemComponent,
-    HeatMapComponent,
-    ResultListComponent,
-    SelectionPipe,
-    ContainerComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    LeafletModule.forRoot(),
-    MatCardModule,
-    MatButtonModule,
-    MatListModule,
-    MatSliderModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LandingPageComponent,
+        FeatureSelectionComponent,
+        FeatureSelectionItemComponent,
+        ResultListComponent,
+        SelectionPipe,
+        ContainerComponent,
+        HeatMapComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        LeafletModule.forRoot(),
+        MatCardModule,
+        MatButtonModule,
+        MatListModule,
+        MatSliderModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
