9da01b5aecc97405ca973e5aee503f4b98c1386e
jkinyanjui <ngeizify@gmail.com>
2019-09-28 11:28:48 +0200

    rebase develop branch

--- src/app/heatmap/heat-map.component.ts
+++ src/app/heatmap/heat-map.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet.heat/dist/leaflet-heat.js';
 import {DataPoint} from '../models/DataPoint';
+import {HomeService} from "../service/home.service";
 
 @Component({
     selector: 'app-heatmap',
@@ -22,9 +23,7 @@ export class HeatMapComponent implements OnInit {
     center: L.latLng(51.820500298442674, 7.382656727408407)
   };
 
-  constructor() {
-    this.dataPoints = HomeService.getDataPoints();
-  }
+  constructor() { }
 
   ngOnInit() { }
 
@@ -34,7 +33,6 @@ export class HeatMapComponent implements OnInit {
             .filter(x => x[2] > 0.0) // todo  choose better weight.
             .map(x => {
               let coord = [x[1], x[0], Math.round(x[2] * 100) / 100];
-              console.log(coord);
               return coord;
             });
     // @ts-ignore
