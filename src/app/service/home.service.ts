9182e3876d96e60be1fda3d569e5c4a7e028e087
Johanna Nolte <johanna.nolte@codecentric.de>
2019-09-28 17:00:26 +0200

    Change scales

--- src/app/service/home.service.ts
+++ src/app/service/home.service.ts
@@ -12,35 +12,35 @@ export class HomeService {
         {
             id: 'near_university',
             label: 'Nähe zur Uni',
-            priorities: [{label: '> 5km', value: 1}, {label: '<=5km', value: 2}, {label: '<1km', value: 3}],
+            priorities: [{label: 'unwichtig', value: 1}, {label: 'mittel', value: 2}, {label: 'wichtig', value: 3}],
             description: 'Hier kannst du einstellen, wie nah an der Uni du wohnen möchtest.',
             value: 1
         },
         {
             id: 'shops',
             label: 'Supermärkte',
-            priorities: [{label: 'weit entfernt', value: 1}, {label: 'moderat', value: 2}, {label: 'nah', value: 3}],
+            priorities: [{label: 'unwichtig', value: 1}, {label: 'mittel', value: 2}, {label: 'wichtig', value: 3}],
             description: 'Wie wichtig sind dir Einkaufsmöglichkeiten in der Nähe?',
             value: 2
         },
         {
             id: 'nightlife',
             label: 'Nightlife',
-            priorities: [{label: 'keine', value: 1}, {label: 'wenige Bars', value: 2}, {label: 'Altstadt', value: 3}],
+            priorities: [{label: 'unwichtig', value: 1}, {label: 'mittel', value: 2}, {label: 'wichtig', value: 3}],
             description: 'Gehst du gerne feiern? Die Münsteraner Altstadt hat ein reges Nachtleben!',
             value: 1
         },
         {
             id: 'public_transport',
             label: 'ÖPNV',
-            priorities: [{label: '> 200m', value: 1}, {label: '<= 200m', value: 2}, {label: '<100m', value: 3}],
+            priorities: [{label: 'unwichtig', value: 1}, {label: 'mittel', value: 2}, {label: 'wichtig', value: 3}],
             description: 'Fährst du gerne mit dem Bus? Wie nah sollte die näheste Bushaltestelle sein?',
             value: 3
         },
         {
             id: 'avg_cost',
             label: 'Mietspiegel',
-            priorities: [{label: 'hoch', value: 1}, {label: 'niedrig', value: 2}],
+            priorities: [{label: 'unwichtig', value: 1}, {label: 'wichtig', value: 2}],
             description: 'Wie viel Budget steht dir für deine neue Wohnung zur Verfügung?',
             value: 2
         }
