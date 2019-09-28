67b08e24bfceb278a018a1f338a704359d23d966
philipp.wolters <philipp.wolters@codecentric.de>
2019-09-28 11:06:21 +0200

    combine features

--- src/app/feature-selection/feature-selection.component.ts
+++ src/app/feature-selection/feature-selection.component.ts
@@ -1,20 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {HomeService} from '../service/home.service';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Feature} from '../models/Feature';
+import {Select} from "../models/Select";
 
 @Component({
-  selector: 'app-feature-selection',
-  templateUrl: './feature-selection.component.html',
-  styleUrls: ['./feature-selection.component.css']
+    selector: 'app-feature-selection',
+    templateUrl: './feature-selection.component.html',
+    styleUrls: ['./feature-selection.component.css']
 })
 export class FeatureSelectionComponent implements OnInit {
-  features: Feature[] = [];
+    @Input() role = [];
+    @Input() features: Feature[] = [];
+    @Output() messageEvent = new EventEmitter<Map<string, number>>();
 
-  constructor(private homeService: HomeService) {
-    this.features = homeService.getFeatureList();
-  }
+    selection: Map<string, number>;
 
-  ngOnInit() {
+    constructor() {
 
-  }
+    }
+
+    ngOnInit() {
+      this.selection = new Map<string, number>();
+      this.features.forEach(f => this.selection.set(f.id, f.value));
+    }
+
+    receiveMessage($event) {
+      let select: Select = $event;
+      this.selection.set(select.label, select.value);
+      this.emit();
+    }
+
+    emit() {
+        this.messageEvent.emit(this.selection);
+    }
 }
